e92ec55c5707854ac0313d96bede4c60666ce0c7
FauzanNA <98865061+FauzanNA@users.noreply.github.com>
2022-03-08 16:13:01 +0700

    Add files via upload

---
+++ components/navbar.vue
@@ -0,0 +1,21 @@
+<template>
+    <main>
+        <nav class="navbar navbar-expand-lg" style="background: #000000;">
+            <a href="#" class="navbar-brand" style="color: white;">FZSTORE</a>
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul class="navbar-nav mr-auto">
+                        <li class="nav-item">
+                            <NuxtLink to="/" class="text-white cd-md-6">
+                                Home
+                            </NuxtLink>
+                        </li>
+                        <li class="nav-item">
+                            <NuxtLink to="/product" class="text-white cd-md-6">
+                                Product
+                            </NuxtLink>
+                        </li>
+                    </ul>
+                </div>
+        </nav>
+    </main>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,8 @@
+<template>
+  <main>
+    <navbar/>
+    <div class="jumbotron">
+      <h1>WELCOME TO MY FZSTORE</h1>
+    </div>
+  </main>
+</template>
